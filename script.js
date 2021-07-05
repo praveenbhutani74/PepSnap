c8fe0b44daa0e248450e7d967b2686697f2a86e1
Praveen <bhutanipraveen74@gmail.com>
2021-07-05 13:24:47 +0530

    Video Object

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 let videoElement=document.querySelector("video");
-let constraint = { video: true };
+let recordButton=document.querySelector("#record");
+let  recordingState=false;
+let mediaRecorder;
 
-navigator.mediaDevices.getUserMedia(constraint).then(
-    function (mediaStream) {
-    videoElement.srcObject=mediaStream;
-      
-    }).catch(
-    function (error) { console.log(error); 
-});
 
+(async function(){
+
+  let constraint = { video: true };
+  let mediaStream= await  navigator.mediaDevices.getUserMedia(constraint)
+   
+   videoElement.srcObject=mediaStream;
+   mediaRecorder=new MediaRecorder(mediaStream);
+
+   mediaRecorder.onstart=function(){
+    console.log("Inside me start");
+   }
+   mediaRecorder.ondataavailable=function(e){
+    console.log("Inside on data available");
+
+    let videoObj= new Blob([e.data],{type:"video/mp4"})
+    console.log(videoObj);
+
+    
+   };
+   
+   mediaRecorder.onstop=function(){
+
+    console.log("Inside on stop");
+
+   }
+
+   recordButton.addEventListener("click",function(){
+
+    if(recordingState){
+        mediaRecorder.stop();
+        recordButton.innerHTML=`Record Video..`
+        recordingState=false;
+
+    }
+    else{
+
+        mediaRecorder.start();
+        recordButton.innerHTML=`Recording Start ....`
+        recordingState=true;
+    }
+
+   })
+
+
+
+})();
