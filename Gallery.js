07ce9960042c860198e0e41137711b93a1f0ad16
Praveen <bhutanipraveen74@gmail.com>
2021-07-08 22:20:41 +0530

    Delete and Download done And complete Project

--- Gallery.js
+++ Gallery.js
@@ -54,6 +54,17 @@ function fetchMedia(){
           <div class="download-media">Download</div>
           <div class="delete-media">Delete</div>
       </div>`;
+      mediaDiv
+    .querySelector(".download-media")
+    .addEventListener("click", function () {
+        console.log("helllo");
+      downloadMedia(mediaObj);
+    });
+  mediaDiv
+    .querySelector(".delete-media")
+    .addEventListener("click", function () {
+      deleteMedia(mediaObj, mediaDiv);
+    });
   
     document.querySelector(".gallery").append(mediaDiv);
  }
@@ -61,11 +72,53 @@ function fetchMedia(){
 
     let mediaDiv = document.createElement("div");
   mediaDiv.classList.add("media-div");
-  mediaDiv.innerHTML = `<video class="media-video" controls  ></video>
+  mediaDiv.innerHTML = `<video class="media-video" controls autoplay loop  ></video>
     <div class="media-buttons">
         <div class="download-media">Download</div>
         <div class="delete-media">Delete</div>
     </div>`;
   mediaDiv.querySelector("video").src = URL.createObjectURL(mediaObj.url);
+  mediaDiv
+  .querySelector(".download-media")
+  .addEventListener("click", function () {
+      console.log("hello");
+    downloadMedia(mediaObj);
+  });
+    mediaDiv
+  .querySelector(".delete-media")
+  .addEventListener("click", function () {
+      console.log("delete");
+    deleteMedia(mediaObj, mediaDiv);
+  });
   document.querySelector(".gallery").append(mediaDiv);
+  }
+
+
+
+  function downloadMedia(mediaObj){
+    let aTag=document.createElement("a");
+    if(mediaObj.type=="photo"){
+        aTag.download=`${mediaObj.mid}.jpg`;
+        aTag.href=mediaObj.url;
+
+    }
+    else{
+        aTag.download=`${mediaObj.mid}.mp4`;
+        aTag.href=URL.createObjectURL(mediaObj.url);
+    }
+    aTag.click();
+  }
+
+  function deleteMedia(mediaObj,mediaDiv){
+
+    let mid=mediaObj.mid;
+    let txnObject =db.transaction("Media","readwrite");
+    let mediaTable =txnObject.objectStore("Media");
+
+    mediaTable.delete(mid);
+
+    mediaDiv.remove();
+
+
+
   }
